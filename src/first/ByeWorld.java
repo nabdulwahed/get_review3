776cefc27306761f06542ca3332e31cca461e326
nour <nour@nourals.com>
2022-08-23 11:33:49 -0400

    added bye world

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,9 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+
+        System.out.println("maybe 5 week of java left");
+        System.out.println("1.5 weeks of git");
+    }
+}
